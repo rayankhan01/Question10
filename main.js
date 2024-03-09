b6d6224ae1bcb5afeff986ade99879b6c3fd32d3
rayankhan01 <rk12172002@gmail.com>
2024-03-09 20:06:21 +0500

    Adding Comments

---
+++ main.js
@@ -0,0 +1,4 @@
+// Rayan, 08-03-2024 //
+var myname = "Rayan";
+var date = "08-03-2024";
+console.log("Hello ".concat(myname + date, ",the day challenge has been received and you have not yet completed the day challenge!"));
